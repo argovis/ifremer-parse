e774b72326d2795dc4cbb9a8603d7b670ee5c4b8
billmills <you@example.com>
2022-03-07 12:59:38 -0500

    roudtrip integrity test

--- roundtrip.py
+++ roundtrip.py
@@ -3,96 +3,7 @@ from geopy import distance
 from itertools import compress
 import numpy.ma as ma
 import wget, xarray, time, re, datetime, math, os, glob
-
-def find_basin(lon, lat):
-    # for a given lon, lat,
-    # identify the basin from the lookup table.
-    # choose the nearest non-nan grid point.
-
-    gridspacing = 0.5
-    basins = xarray.open_dataset('parameters/basinmask_01.nc')
-
-    basin = basins['BASIN_TAG'].sel(LONGITUDE=lon, LATITUDE=lat, method="nearest").to_dict()['data']
-    if math.isnan(basin):
-        # nearest point was on land - find the nearest non nan instead.
-        lonplus = math.ceil(lon / gridspacing)*gridspacing
-        lonminus = math.floor(lon / gridspacing)*gridspacing
-        latplus = math.ceil(lat / gridspacing)*gridspacing
-        latminus = math.floor(lat / gridspacing)*gridspacing
-        grids = [(basins['BASIN_TAG'].sel(LONGITUDE=lonminus, LATITUDE=latminus, method="nearest").to_dict()['data'], distance.distance((lat, lon), (latminus, lonminus)).miles),
-                 (basins['BASIN_TAG'].sel(LONGITUDE=lonminus, LATITUDE=latplus, method="nearest").to_dict()['data'], distance.distance((lat, lon), (latplus, lonminus)).miles),
-                 (basins['BASIN_TAG'].sel(LONGITUDE=lonplus, LATITUDE=latplus, method="nearest").to_dict()['data'], distance.distance((lat, lon), (latplus, lonplus)).miles),
-                 (basins['BASIN_TAG'].sel(LONGITUDE=lonplus, LATITUDE=latminus, method="nearest").to_dict()['data'], distance.distance((lat, lon), (latminus, lonplus)).miles)]
-
-        grids = [x for x in grids if not math.isnan(x[0])]
-        if len(grids) == 0:
-            # all points on land
-            #print('warning: all surrounding basin grid points are NaN')
-            basin = -1
-        else:
-            grids.sort(key=lambda tup: tup[1])
-            basin = grids[0][0]
-    basins.close()
-    return int(basin)
-
-def argo_keymapping(nckey):
-    # argo netcdf measurement name -> argovis measurement name
-
-    key_mapping = {
-        "PRES": "pres",
-        "TEMP": "temp",
-        "PSAL": "psal",
-        "CNDX": "cndx",
-        "DOXY": "doxy",
-        "CHLA": "chla",
-        "CDOM": "cdom",
-        "NITRATE": "nitrate",
-        "BBP700": "bbp700",
-        "DOWN_IRRADIANCE412": "down_irradiance412",
-        "DOWN_IRRADIANCE442": "down_irradiance442",
-        "DOWN_IRRADIANCE490": "down_irradiance490",
-        "DOWNWELLING_PAR": "downwelling_par",
-        "PRES_QC": "pres_qc",
-        "TEMP_QC": "temp_qc",
-        "PSAL_QC": "psal_qc",
-        "CNDX_QC": "cndx_qc",
-        "DOXY_QC": "doxy_qc",
-        "CHLA_QC": "chla_qc",
-        "CDOM_QC": "cdom_qc",
-        "NITRATE_QC": "nitrate_qc",
-        "BBP700_QC": "bbp700_qc",
-        "DOWN_IRRADIANCE412_QC": "down_irradiance412_qc",
-        "DOWN_IRRADIANCE442_QC": "down_irradiance442_qc",
-        "DOWN_IRRADIANCE490_QC": "down_irradiance490_qc",
-        "DOWNWELLING_PAR_QC": "downwelling_par_qc"
-    }
-
-    try:
-        argoname = key_mapping[nckey.replace('_ADJUSTED', '')]
-    except:
-        #print('warning: unexpected variable found in station_parameters:', nckey)
-        argoname = nckey.replace('_ADJUSTED', '').lower()
-
-    return argoname
-
-def cleanup(meas):
-    # given a measurement, return the measurement after some generic cleanup
-
-    if meas is None:
-        return meas
-
-    # qc codes come in as bytes, should be ints
-    try:
-        ducktype = meas.decode('UTF-8')
-        return int(meas)
-    except:
-        pass
-
-    # use None as missing fill
-    if math.isnan(meas):
-        return None        
-
-    return round(meas,6) # at most 6 significant decimal places
+import util.helpers as h
 
 client = MongoClient('mongodb://database/argo')
 db = client.argo
@@ -100,10 +11,7 @@ db = client.argo
 while True:
 	time.sleep(60)
 	p = list(db.profs.aggregate([{"$sample": {"size": 1}}]))[0]
-	#p = db.profs.find_one({"_id":"5900476_000"}) # contains nans
-	#p = db.profs.find_one({"_id":"5900475_017"}) # no nans, but a pres=0.0 level that should be retained
-	#p = db.profs.find_one({"_id":"2900205_327"}) # possible mismatch, check again
-	#2900918_035
+	#p = db.profs.find_one({"_id":"5900476_000"})
 
 	p_lookup = {level[p['data_keys'].index('pres')]: ma.masked_array(level, [False]*len(level)) for level in p['data']} # transform argovis profile data into pressure-keyed lookup table of levels with values sorted as data_keys. Levels are initialized as masked arrays with no elements masked.
 	nc = []
@@ -145,7 +53,7 @@ while True:
 		if p['_id'] != reconstruct_id:
 			print('profile _id mangled for', xar['source'], p['_id'], reconstruct_id)
 
-		if p['basin'] != find_basin(xar['data']['LONGITUDE'].to_dict()['data'][0], xar['data']['LATITUDE'].to_dict()['data'][0]):
+		if p['basin'] != h.find_basin(xar['data']['LONGITUDE'].to_dict()['data'][0], xar['data']['LATITUDE'].to_dict()['data'][0]):
 			print('basin mismatch at', xar['source'])
 
 		if p['data_type'] != 'oceanicProfile':
@@ -233,13 +141,13 @@ while True:
 
 			nc_data = list(zip(*[xar['data'][var].to_dict()['data'][0] for var in data_sought])) # all the upstream data, packed in a list of levels (sorted by original nc sort order, not necessarily depth), each of which is a list of values sorted as data_sought
 			for level in nc_data:
-				pressure = cleanup(level[data_sought.index(nc_pressure_label)])
+				pressure = h.cleanup(level[data_sought.index(nc_pressure_label)])
 				if pressure is None:
 					continue # summarily drop any level that doesn't have a meaningful pressure
 				elif pressure in p_lookup:
 					for nc_key in data_sought:
-						nc_val = cleanup(level[data_sought.index(nc_key)])
-						av_idx = p['data_keys'].index(argo_keymapping(nc_key))
+						nc_val = h.cleanup(level[data_sought.index(nc_key)])
+						av_idx = p['data_keys'].index(h.argo_keymapping(nc_key))
 						av_val = p_lookup[pressure][av_idx]
 						if nc_val != av_val:
 							print(f'data mismatch at {nc_key} and pressure {pressure} in {xar["source"]}')
@@ -247,8 +155,6 @@ while True:
 							p_lookup[pressure].mask[av_idx] = True # mask out any measurements found in both nc and mongo
 				else:
 					print(f'pressure {pressure} not found in argovis profile from sourcefile {xar["source"]}')
-			print(p['data'])
-			print(nc_data)
 
 		elif prefix in ['SD', 'SR']:
 			# check bgc / synth data
@@ -275,13 +181,13 @@ while True:
 
 			nc_data = list(zip(*[xar['data'][var].to_dict()['data'][0] for var in data_sought])) # all the upstream data, packed in a list of levels (sorted by original nc sort order, not necessarily depth), each of which is a list of values sorted as data_sought
 			for level in nc_data:
-				pressure = cleanup(level[data_sought.index(nc_pressure_label)])
+				pressure = h.cleanup(level[data_sought.index(nc_pressure_label)])
 				if pressure is None:
 					continue # summarily drop any level that doesn't have a meaningful pressure
 				elif pressure in p_lookup:
 					for nc_key in data_sought:
-						nc_val = cleanup(level[data_sought.index(nc_key)])
-						av_idx = p['data_keys'].index(argo_keymapping(nc_key).replace('temp', 'temp_sfile').replace('psal', 'psal_sfile'))
+						nc_val = h.cleanup(level[data_sought.index(nc_key)])
+						av_idx = p['data_keys'].index(h.argo_keymapping(nc_key).replace('temp', 'temp_sfile').replace('psal', 'psal_sfile'))
 						av_val = p_lookup[pressure][av_idx]
 						if nc_val != av_val:
 							print(f'data mismatch at {nc_key} and pressure {pressure} in {xar["source"]}')
@@ -292,11 +198,13 @@ while True:
 		else:
 			print(f'unexpected prefix {prefix} found')
 
-	# if the argovis profile matches the netcdf exactly, then p_lookup should have nothing but completely masked arrays left:
+	# if the argovis profile matches the netcdf exactly, then p_lookup should have nothing but masked values and Nones left:
 	for level in p_lookup:
 		if not p_lookup[level].mask.all():
-			print(f'unmasked value in {p_lookup[level]} at profile {p["_id"]}')
-			print(p['data_keys'])
+			leftovers = p_lookup[level][p_lookup[level].mask == False]
+			if not all(v is None for v in leftovers):
+				print(f'unmasked, non-None value in {p_lookup[level]} at profile {p["_id"]}')
+				print(p['data_keys'])
 
 	for f in glob.glob("*.nc"):
 		os.remove(f)
